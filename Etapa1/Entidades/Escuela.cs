2f74d73433529e6dccb435a5bdb8a5b985cd6c73
Rafael Rubiano <rafaelrubiano@Rafaels-MacBook-Pro.local>
2022-04-10 23:24:47 -0500

    Se guardan los cambios de la clase 11

--- Etapa1/Entidades/Escuela.cs
+++ Etapa1/Entidades/Escuela.cs
@@ -1,4 +1,16 @@
 namespace CoreEscuela.Entidades
 {
-    
+    class Escuela
+    {
+        string nombre;
+        public string Nombre
+        {
+            get{ return "Copia:" + nombre; }
+            set{ nombre = value.ToUpper(); }
+        }
+        public int AñoDeCreación { get; set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
+    }
 }
--- Etapa1/Program.cs
+++ Etapa1/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CoreEscuela.Entidades;
+class program
+{
+    static void Main(string[] args)
+    {
+        var escuela = new Escuela("Platzi Academy", 2012);
+        Console.WriteLine(escuela.Nombre);
+    }
+}
+
+
