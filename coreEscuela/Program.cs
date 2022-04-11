ce3b08ef9a31466a850edaccc9bec0123df74d1b
Rafael Rubiano <rafaelrubiano@Rafaels-MacBook-Pro.local>
2022-04-10 22:37:23 -0500

    Se guardan cambios de la clase 9

--- coreEscuela/Program.cs
+++ coreEscuela/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace CoreEscuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        //public string ceo;
+       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Platzi Academy";
+            miEscuela.direccion = "Cra 9 calle 72";
+            miEscuela.añoFundacion = 2012;
+            Console.WriteLine("Timbre");
+            //Console.WriteLine("Hello, World!");
+
+        }
+    }
+
+}
